50354b010765ae1474319733ba8ab6c9dcef3724
Rhythm1027 <95673596+Rhythm1027@users.noreply.github.com>
2021-12-07 11:49:06 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+float a,b;
+cout<<"Enter two numbers: ";
+cin>>a;
+cin>>b;
+float multiply=a*b;
+cout<<"Product = "<<multiply;
+return 0;
+}
---
+++ size.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+char c;
+int a;
+float b;
+double d;
+cout<<"The size of char: "<<sizeof(c)<<" bytes"<<endl;
+cout<<"The size of int: "<<sizeof(a)<<" bytes"<<endl;
+cout<<"The size of float: "<<sizeof(b)<<" bytes"<<endl;
+cout<<"The size of double: "<<sizeof(d)<<" bytes"<<endl;
+return 0;
+}
